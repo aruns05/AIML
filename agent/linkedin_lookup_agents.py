aba972562142bbfd23ae5419b80cfe10ef6fed59
aruns05 <arun.s2k8@gmail.com>
2024-10-14 23:38:44 +0530

    langchian agent

--- agent/linkedin_lookup_agents.py
+++ agent/linkedin_lookup_agents.py
@@ -1,9 +1,7 @@
-
-
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.tools import Tool
-from langchain.agents  import (
+from langchain.agents import (
     create_react_agent,
     AgentExecutor,
 )
@@ -15,51 +13,45 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def lookup(name: str) -> str:
-    OPENAI_API_KEY =  os.environ['OPENAI_API_KEY']
-    print('OPENAI_API_KEY', OPENAI_API_KEY)
+    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+    print("OPENAI_API_KEY", OPENAI_API_KEY)
     llm = ChatOpenAI(
         temperature=0,
         model="gpt-3.5-turbo",
     )
     template = """
-    Given the full name {name_of_person} I want you to get me link to their Linkedin Profile page. {name_of_person} is a architect and works in Cognizant Bengaluru  . Your answer should
+    Given the full name {name_of_person} I want you to get me link to their Linkedin Profile page. {name_of_person} is a software architect who works with Cognizant Technologies Bengaluru  . Your answer should
     contain only a url
     """
     prompt_template = PromptTemplate(
-        template=template,
-        input_variables=["name_of_person"]
+        template=template, input_variables=["name_of_person"]
     )
-    
-    tools_for_agent =[
+
+    tools_for_agent = [
         Tool(
-            name="Crawl google 4 linkedin profiles with location Bengaluru",
+            name="Crawl google 10 linkedin profiles",
             func=get_profile_url,
-            description="Useful for when you need to get the linkedin url"
+            description="Useful for when you need to get the linkedin url",
         )
     ]
 
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
 
-    react_prompt= hub.pull("hwchase17/react")
-    agent = create_react_agent(llm=llm,tools=tools_for_agent,prompt=react_prompt)
-    agent_executor = AgentExecutor(agent=agent,tools=tools_for_agent, verbose=True)
-    
     result = agent_executor.invoke(
-        input ={
-            "input": prompt_template.format_prompt(name_of_person=name)
-        }
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
     )
-    
+
     linked_profile_url = result["output"]
     return linked_profile_url
-    
-    
-    #LANGCHAIN O/P PARSER
-    #return "https://www.linkedin.com/in/arun-sridhar-7402991a/"
 
-if __name__ =="__main__":
-    print('here')
+    # LANGCHAIN O/P PARSER
+    # return "https://www.linkedin.com/in/arun-sridhar-7402991a/"
+
+
+if __name__ == "__main__":
     print(lookup("Arun Sridhar"))
-    #link='https://gist.githubusercontent.com/aruns05/8c303b5aa3527f313f2efcc6c3bbece1/raw/e4e8104decd3bd9ba5b1be0da5ed57d81614ac29/gistfile1.txt'
-    
-    
+    # link='https://gist.githubusercontent.com/aruns05/8c303b5aa3527f313f2efcc6c3bbece1/raw/e4e8104decd3bd9ba5b1be0da5ed57d81614ac29/gistfile1.txt'
---
+++ agent/main.py
@@ -0,0 +1,89 @@
+from typing import Union, List
+
+from dotenv import load_dotenv
+from langchain.tools import Tool, tool
+from langchain.tools.render import render_text_description
+from langchain_core.prompts import PromptTemplate
+from langchain.schema import AgentAction, AgentFinish
+from langchain_openai import ChatOpenAI
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+
+from callback import AgentCallBackHandler 
+
+load_dotenv()
+
+
+# tool decorator is a langchain utility function, it is used to create tool.
+@tool
+def get_text_length(text: str) -> int:
+    """Returns the length of a text by characters"""
+    text = text.strip("\n").strip('"')
+    return len(text)
+
+@tool
+def sum_2_numbers(x1: int, x2:int) ->int:
+     """Returns the sum of 2 numbers"""
+     return x1+x2
+
+@tool
+def generate_jira_stories(description : str) -> str:
+    """Returns the user story and acceptance criteria based on input given"""
+    return "" 
+    
+
+
+    
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool wtih name {tool_name} not found")
+
+if __name__ == "__main__":
+    print("Hello ReAct Langchain")
+    tools = [get_text_length, sum_2_numbers, generate_jira_stories]
+    
+    template ="""
+        Answer the following questions as best you can. You have access to the following tools:
+        {tools}
+        Use the following format:
+        Question: the input question you must answer
+        Thought: you should always think about what to do
+        Action: the action to take, should be one of [{tool_names}]
+        Action Input: the input to the action
+        Observation: the result of the action
+        ... (this Thought/Action/Action Input/Observation can repeat N times)
+        Thought: I now know the final answer
+        Final Answer: the final answer to the original input question
+        Begin!
+        Question: {input}
+        Thought:{agent_scratchpad}
+    """
+    
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),tool_names =",".join([t.name for t in tools])
+    )
+    
+    llm = ChatOpenAI(temperature=0,stop=["\nObservation"], callbacks=[AgentCallBackHandler()])
+    intermediate_steps =[]
+    agent = {"input":lambda x:x["input"], "agent_scratchpad": lambda x:x["agent_scratchpad"]}|prompt | llm | ReActSingleInputOutputParser()
+    #agent = {"input":lambda x:x["input"]}|prompt | llm | ReActSingleInputOutputParser()
+
+    
+    print('intermediate_steps', intermediate_steps)
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+                                                                {
+                                                                    "input":"What is the length of 'DOG' in characters?",
+                                                                    "agent_scratchpad":intermediate_steps,
+                                                                }
+                                                            )
+    print(agent_step)
+    
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+
+        observation = tool_to_use.func(str(tool_input))
+        intermediate_steps.append((agent_step, str(observation)))
+        print(f"{observation=}")
